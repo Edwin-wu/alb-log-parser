40d1a17848def1319ed345827b444f32e340ba0f
Edwin wu <chunpu@163.com>
2020-10-29 17:07:11 +0800

    Add files via upload

--- alb-log-to-es-sample.sh
+++ alb-log-to-es-sample.sh
@@ -8,6 +8,8 @@ ES_DOMAIN_NAME=example-domain
 ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name $ES_DOMAIN_NAME --query "DomainStatus.Endpoint" --output text)
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
+
+#创建 Lambda 的角色和权限
 cd /tmp
 if [ ! -f "/tmp/alb-log-s3-reader.json" ];then
   echo "创建 Lambda 的权限配置文件"
@@ -24,7 +26,7 @@ echo -n "{
                 \"s3:GetObject\", 
                 \"s3:ListBucket\" 
             ], 
-            \"Resource\": \"arn:aws-cn:s3:::${LOG_BUCKET_NAME}\"
+            \"Resource\": \"arn:aws-cn:s3:::${LOG_BUCKET_NAME}/*\"
         },
         {
             \"Effect\": \"Allow\",
@@ -43,16 +45,19 @@ echo -n "{
           }
     ]
 }">>alb-log-s3-reader.json
-wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/lambda-trustpolicy.json -O lambda-trustpolicy.json
+wget https://raw.githubusercontent.com/Edwin-wu/alb-log-parser/master/lambda-trustpolicy.json -O lambda-trustpolicy.json
 echo "正在为Lambda创建 IAM角色....."
 if aws iam create-role --role-name alb-log-s3-reader --assume-role-policy-document file://lambda-trustpolicy.json ; then
     if aws iam put-role-policy --role-name alb-log-s3-reader --policy-name alb-log-s3-reader --policy-document file://alb-log-s3-reader.json ; then
         role_arn=$(aws iam get-role --role-name alb-log-s3-reader --query "Role.Arn" --output text)
+        policy_arn=
         echo " Lambda角色创建完毕，已成功关联 S3 权限。"
         rm alb-log-s3-reader.json lambda-trustpolicy.json
     else
         echo "Lambda角色创建完毕，但是关联 S3 权限失败。"
         rm alb-log-s3-reader.json lambda-trustpolicy.json
+        aws iam delete-role-policy --role-name alb-log-s3-reader --policy-name alb-log-s3-reader
+        aws iam delete-role --role-name alb-log-s3-reader
         exit 1
     fi
 else
@@ -60,23 +65,30 @@ else
     rm alb-log-s3-reader.json lambda-trustpolicy.json
     exit 1
 fi
-echo "开始创建 Lambda 函数......"
-wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/ALB-log-parsing.zip -O ALB-log-parsing.zip
+
+#创建 Lambda 函数
+echo "开始下载 Lambda 代码......"
+wget https://raw.githubusercontent.com/Edwin-wu/alb-log-parser/master/ALB-log-processor.zip -O ALB-log-processor.zip
+echo "开始上传 Lambda 代码并创建函数......"
 if aws lambda create-function \
         --function-name ALB-log-parsing \
         --runtime python3.7 \
         --zip-file fileb://ALB-log-parsing.zip \
-        --handler lambda_function.lambda_handler \
+        --handler log_parser.lambda_handler \
         --role $role_arn \
         --timeout 600 \
-    --environment "Variables={ES_ENDPOINT=$ES_ENDPOINT}" ; then
+    --environment "Variables={ELASTIC_ENDPOINT=$ES_ENDPOINT}" ; then
     lambda_arn=$(aws lambda get-function --function-name  ALB-log-parsing --query "Configuration.FunctionArn" --output text)
     echo " Lambda 创建完毕，正在为您 配置 S3 事件触发Lambda....."
 else
-    echo "创建 Lambda 函数失败，请检查LOG_BUCKET_NAME以及ES_DOMAIN_NAME是否已经配置正确，是否已有重名的 Lambda或者您是否有权限创建Lambda 函数"
+    echo "创建 Lambda 函数失败，请检查是否已有重名的 Lambda或者您是否有权限创建Lambda 函数"
+    aws iam delete-role-policy --role-name alb-log-s3-reader --policy-name alb-log-s3-reader
+    aws iam delete-role --role-name alb-log-s3-reader
     rm ALB-log-parsing.zip
     exit 1
 fi
+
+#配置 S3 存储桶自动触发 lambda
 echo -n "{
     \"LambdaFunctionConfigurations\": [
         {
@@ -92,9 +104,34 @@ if aws lambda add-permission --function-name ALB-log-parsing --principal s3.amaz
     --source-arn arn:aws-cn:s3:::$LOG_BUCKET_NAME \
     --source-account $AWS_ACCOUNT_ID ; then
     aws s3api put-bucket-notification-configuration --bucket $LOG_BUCKET_NAME --notification-configuration file://notification.json
-    echo "Lambda和S3已经配置完毕，请在 Amazon ElasticSearch 中继续配置权限，允许 Lambda 写入"
+    echo "Lambda和S3已经配置完毕，即将在 Amazon ElasticSearch 中配置权限，允许 Lambda 写入"
 else
-    echo "配置 S3 时间触发 Lambda，请确认您是否有权限配置 S3 的事件触发器！"
-    rm notification.json
+    echo "配置 S3 事件触发 Lambda失败，请确认您是否有权限配置 S3 的事件触发器！"
+    aws iam delete-role-policy --role-name alb-log-s3-reader --policy-name alb-log-s3-reader
+    aws iam delete-role --role-name alb-log-s3-reader
+    aws lambda delete-function --function-name ALB-log-parsing
+    rm notification.json ALB-log-parsing.zip
     exit 1
 fi
+
+#配置 ElasticSearch 服务以添加 Lambda role 的权限
+aws es describe-elasticsearch-domain --domain-name $ES_DOMAIN_NAME --query "DomainStatus.AccessPolicies" --output text >>old_policy.json
+if jq .Statement[0].Principal.AWS[0] old_policy.json >/dev/null 2>&1; then
+    new_principal=$(jq .Statement[0].Principal.AWS+[\"$role_arn\"] old_policy.json)
+else
+    new_principal=$(jq [.Statement[0].Principal.AWS]+[\"$role_arn\"] old_policy.json)
+fi
+new_policy=$(jq .Statement[0].Principal.AWS="$new_principal" old_policy.json --compact-output)
+if aws es update-elasticsearch-domain-config --domain-name $ES_DOMAIN_NAME --access-policies $new_policy; then
+    echo "已经完成更新 ElasticSearch 的权限设置，请继续登录ElasticSearch设置"
+    rm old_policy.json
+else
+    echo "更新 ElasticSearch 权限设置失败，请检查 ElasticSearch 是否开启了精细化权限控制，或者 ElasticSearch是否处于 VPC 中"
+    rm old_policy.json
+    rm notification.json ALB-log-parsing.zip 
+    exit 1
+fi
+rm notification.json ALB-log-parsing.zip
+
+
+

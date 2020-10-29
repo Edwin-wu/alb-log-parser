4fc6858cbc61799cba0b09bfbdcb1d89a4268494
Edwin wu <chunpu@163.com>
2020-10-29 16:43:36 +0800

    Add files via upload

--- alb-log-to-es-sample.sh
+++ alb-log-to-es-sample.sh
@@ -43,7 +43,7 @@ echo -n "{
           }
     ]
 }">>alb-log-s3-reader.json
-wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/lambda-trustpolicy.json
+wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/lambda-trustpolicy.json -O lambda-trustpolicy.json
 echo "正在为Lambda创建 IAM角色....."
 if aws iam create-role --role-name alb-log-s3-reader --assume-role-policy-document file://lambda-trustpolicy.json ; then
     if aws iam put-role-policy --role-name alb-log-s3-reader --policy-name alb-log-s3-reader --policy-document file://alb-log-s3-reader.json ; then
@@ -61,7 +61,7 @@ else
     exit 1
 fi
 echo "开始创建 Lambda 函数......"
-wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/ALB-log-parsing.zip
+wget https://edwin-blog.s3.cn-northwest-1.amazonaws.com.cn/ALB-log-parsing.zip -O ALB-log-parsing.zip
 if aws lambda create-function \
         --function-name ALB-log-parsing \
         --runtime python3.7 \
